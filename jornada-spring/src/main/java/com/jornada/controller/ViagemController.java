4c1fef203a58af37a5f539f15c01f2aecaa71699
CRFlam <jjsmithcrf@gmail.com>
2023-12-15 21:55:13 -0300

    Inclusão de editar nas passagens

--- jornada-spring/src/main/java/com/jornada/controller/ViagemController.java
+++ jornada-spring/src/main/java/com/jornada/controller/ViagemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,67 +24,88 @@ import com.jornada.service.ViagemService;
 @Controller
 public class ViagemController {
 
-    @Autowired
-    private ViagemService viagemService;
+	@Autowired
+	private ViagemService viagemService;
 
-    @Autowired
-    private DestinoService destinoService;
+	@Autowired
+	private DestinoService destinoService;
 
-    @Autowired
-    private PassageiroService passageiroService;
+	@Autowired
+	private PassageiroService passageiroService;
 
-    @GetMapping("/cadastrarViagem")
-    public ModelAndView cadastrarViagem() {
-        ModelAndView modelAndView = new ModelAndView("cadastrarViagem");
+	@GetMapping("/cadastrarViagem")
+	public ModelAndView cadastrarViagem() {
+		ModelAndView modelAndView = new ModelAndView("cadastrarViagem");
 
-        List<Destino> destinos = destinoService.listarDestinos();
-        List<Passageiro> passageiros = passageiroService.listarPassageiros();
+		List<Destino> destinos = destinoService.listarDestinos();
+		List<Passageiro> passageiros = passageiroService.listarPassageiros();
 
-        modelAndView.addObject("destinos", destinos);
-        modelAndView.addObject("passageiros", passageiros);
-        modelAndView.addObject("viagem", new Viagem());
+		modelAndView.addObject("destinos", destinos);
+		modelAndView.addObject("passageiros", passageiros);
+		modelAndView.addObject("viagem", new Viagem());
 
-        return modelAndView;
-    }
+		return modelAndView;
+	}
 
-    @PostMapping("/cadastrarViagem")
-    public ModelAndView cadastrarViagem(Viagem viagem,
-                                        @RequestParam("idDestino") Long idDestino,
-                                        @RequestParam("idPassageiro") Long idPassageiro,
-                                        @RequestParam("dataHoraIda") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataHoraIda,
-                                        @RequestParam("dataHoraVolta") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataHoraVolta) {
+	@PostMapping("/cadastrarViagem")
+	public ModelAndView cadastrarViagem(Viagem viagem, @RequestParam("idDestino") Long idDestino,
+			@RequestParam("idPassageiro") Long idPassageiro,
+			@RequestParam("dataHoraIda") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataHoraIda,
+			@RequestParam("dataHoraVolta") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataHoraVolta) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/listarViagens");
+
+		Destino destino = destinoService.obterDestinoPorId(idDestino);
+		Passageiro passageiro = passageiroService.obterPassageiroPorId(idPassageiro);
+
+		viagem.setDestino(destino);
+		viagem.setPassageiro(passageiro);
+		viagem.setDataHoraIda(dataHoraIda);
+		viagem.setDataHoraVolta(dataHoraVolta);
+
+		viagemService.salvarViagem(viagem);
+
+		return modelAndView;
+	}
+
+	@GetMapping("/{id}/editarViagem")
+	public ModelAndView editarViagem(@PathVariable Long id) {
+		ModelAndView modelAndView = new ModelAndView("editarViagem");
+
+		Viagem viagem = viagemService.obterViagemPorId(id);
+		List<Destino> destinos = destinoService.listarDestinos();
+		List<Passageiro> passageiros = passageiroService.listarPassageiros();
+
+		modelAndView.addObject("viagem", viagem);
+		modelAndView.addObject("destinos", destinos);
+		modelAndView.addObject("passageiros", passageiros);
+
+		return modelAndView;
+	}
+
+	@PostMapping("/{id}/editarViagem")
+	public ModelAndView editarViagem(Viagem viagem) { 
+		viagemService.salvarViagem(viagem);
         ModelAndView modelAndView = new ModelAndView("redirect:/listarViagens");
 
-        Destino destino = destinoService.obterDestinoPorId(idDestino);
-        Passageiro passageiro = passageiroService.obterPassageiroPorId(idPassageiro);
+		return modelAndView;
+	}
 
-        viagem.setDestino(destino);
-        viagem.setPassageiro(passageiro);
-        viagem.setDataHoraIda(dataHoraIda);
-        viagem.setDataHoraVolta(dataHoraVolta);
+	@GetMapping("/{id}/excluirViagem")
+	public ModelAndView excluirViagem(@PathVariable Long id) {
+		viagemService.excluirViagem(id);
+		ModelAndView modelAndView = new ModelAndView("redirect:/listarViagens");
 
-        viagemService.salvarViagem(viagem);
+		return modelAndView;
+	}
 
-        return modelAndView;
-    }
+	@RequestMapping("/listarViagens")
+	@GetMapping
+	public ModelAndView listarViagens() {
+		ModelAndView modelAndView = new ModelAndView("listarViagens");
 
+		List<Viagem> viagens = viagemService.listarViagens();
+		modelAndView.addObject("viagens", viagens);
 
-    @GetMapping("/{id}/excluirViagem")
-    public ModelAndView excluirViagem(@PathVariable Long id) {
-        viagemService.excluirViagem(id);
-        ModelAndView modelAndView = new ModelAndView("redirect:/listarViagens");
-
-        return modelAndView;
-    }
-
-    @RequestMapping("/listarViagens")
-    @GetMapping
-    public ModelAndView listarViagens() {
-        ModelAndView modelAndView = new ModelAndView("listarViagens");
-
-        List<Viagem> viagens = viagemService.listarViagens();
-        modelAndView.addObject("viagens", viagens);
-
-        return modelAndView;
-    }
+		return modelAndView;
+	}
 }
